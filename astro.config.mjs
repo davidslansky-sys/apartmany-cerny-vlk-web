9f9741325eaa36504e56e97efdb93d9376ce63aa
davidslansky-sys <david.slansky@gmail.com>
2026-06-10 07:44:35 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://www.apartmanycernyvlk.cz',
+  trailingSlash: 'always',
   i18n: {
     defaultLocale: 'cs',
     locales: ['cs', 'de', 'en'],

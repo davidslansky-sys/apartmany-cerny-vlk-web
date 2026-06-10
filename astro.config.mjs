a68e0b26ecdc2863caafd363132d717a09bb7d46
davidslansky-sys <david.slansky@gmail.com>
2026-06-10 02:01:13 +0000

    Remove sitemap integration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.apartmanycernyvlk.cz',
-  integrations: [sitemap()],
   i18n: {
     defaultLocale: 'cs',
     locales: ['cs', 'de', 'en'],
